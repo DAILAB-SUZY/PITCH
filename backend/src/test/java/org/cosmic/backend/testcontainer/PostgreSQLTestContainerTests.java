f9791edf3f28df277a07be164aa89420a890470b
BiteSnail <rkwoal216@gmail.com>
2024-04-15 16:13:56 +0900

    test: postgreSQL test container 연결 테스트 추가

---
+++ backend/src/test/java/org/cosmic/backend/testcontainer/PostgreSQLTestContainerTests.java
@@ -0,0 +1,40 @@
+package org.cosmic.backend.testcontainer;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Log4j2
+public class PostgreSQLTestContainerTests {
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
+    @BeforeAll
+    static void beforeAll(){
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll(){
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    public void testConnect(){
+        assertEquals(postgres.getContainerInfo().getState().getStatus(), "running");
+    }
+}
