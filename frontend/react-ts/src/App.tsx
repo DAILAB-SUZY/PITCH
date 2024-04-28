7f1c47d5197fa18fe53e658d07afac1ea3d1e143
ljs831 <ljsljs2580@gmail.com>
2024-04-28 22:18:03 +0900

    feat: styled component 적용

--- frontend/react-ts/src/App.tsx
+++ frontend/react-ts/src/App.tsx
@@ -1,7 +1,23 @@
+import styled from "styled-components";
+
+const Title = styled.div`
+  font-size: ${(props) => props.fontSize};
+  margin: ${(props) => props.margin};
+`;
+
 function App() {
   console.log(1);
 
-  return <div>hello world</div>;
+  return (
+    <div>
+      hello world
+      <div>hi</div>
+      <Title fontSize="45px" margin="0px">
+        {" "}
+        test{" "}
+      </Title>
+    </div>
+  );
 }
 
 export default App;
