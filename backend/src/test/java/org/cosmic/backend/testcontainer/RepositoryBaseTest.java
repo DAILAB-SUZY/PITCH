adc3b15ba918df85a2ab47dec3dd1d786e31fca2
BiteSnail <rkwoal216@gmail.com>
2024-04-30 15:36:47 +0900

    refactor: TestContainer 코드 수정

--- backend/src/test/java/org/cosmic/backend/testcontainer/RepositoryBaseTest.java
+++ backend/src/test/java/org/cosmic/backend/testcontainer/RepositoryBaseTest.java
@@ -1,15 +1,11 @@
 package org.cosmic.backend.testcontainer;
 
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -28,7 +24,7 @@ public abstract class RepositoryBaseTest {
         public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
             TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
                     applicationContext,
-                    "spring.test.database.replace=none", // Tells Spring Boot not to start in-memory db for tests.
+                    "spring.test.database.replace=none",
                     "spring.datasource.url=" + postgres.getJdbcUrl(),
                     "spring.datasource.username=" + postgres.getUsername(),
                     "spring.datasource.password=" + postgres.getPassword()
