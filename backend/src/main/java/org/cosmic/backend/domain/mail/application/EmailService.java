05589b0a055a37bae2798dd472c7dae58d4f9578
BiteSnail <rkwoal216@gmail.com>
2024-05-02 16:03:01 +0900

    feat: 계정 존재 시 오류 기능 구현

--- backend/src/main/java/org/cosmic/backend/domain/mail/application/EmailService.java
+++ backend/src/main/java/org/cosmic/backend/domain/mail/application/EmailService.java
@@ -1,17 +1,33 @@
 package org.cosmic.backend.domain.mail.application;
 
 import lombok.RequiredArgsConstructor;
+import org.cosmic.backend.domain.mail.exceptions.ExistEmailException;
 import org.cosmic.backend.domain.mail.utils.MailContentGenerator;
+import org.cosmic.backend.domain.user.domain.Email;
+import org.cosmic.backend.domain.user.domain.User;
+import org.cosmic.backend.domain.user.repository.EmailRepository;
+import org.cosmic.backend.domain.user.repository.UsersRepository;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
     private final MailContentGenerator mailContentGenerator = new MailContentGenerator();
+    private final EmailRepository emailRepository;
+    private final UsersRepository usersRepository;
 
-    public void sendVerificationEmail(String to, String content){
+    public void sendVerificationEmail(String to, String content) {
+        if(!emailRepository.existsById(to)){
+            emailRepository.save(Email.builder().email(to).verificationCode(content).build());
+        }
+        Optional<User> user = usersRepository.findByEmail(to);
+        if(user.isPresent()){
+            throw new ExistEmailException();
+        }
         mailSender.send(mailContentGenerator.verificationMessage(to, content));
     }
 
