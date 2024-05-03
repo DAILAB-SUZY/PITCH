b53e860d5ba9ff6e5b676c236467c797237b51c1
김준호 <kimjunho1231@naver.com>
2024-05-03 17:28:26 +0900

    refactor: update to function in repository

--- backend/src/main/java/org/cosmic/backend/domain/user/repository/UsersRepository.java
+++ backend/src/main/java/org/cosmic/backend/domain/user/repository/UsersRepository.java
@@ -1,10 +1,11 @@
 package org.cosmic.backend.domain.user.repository;
 
+import org.cosmic.backend.domain.user.domain.Email;
 import org.cosmic.backend.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<User,Long> {
-    Optional<User> findByEmail(String email);//이메일로 찾기
+    Optional<User> findByEmail_Email(String email);//이메일로 찾기
     Boolean findByPassword(String password);
 }
