a9445c3e088f5caf0b940d219227ef2a270230aa
김준호 <kimjunho1231@naver.com>
2024-05-07 19:17:25 +0900

    update: 인증 서비스내부 오류 기능 적용

--- backend/src/main/java/org/cosmic/backend/domain/user/service/UserService.java
+++ backend/src/main/java/org/cosmic/backend/domain/user/service/UserService.java
@@ -1,9 +1,16 @@
 package org.cosmic.backend.domain.user.service;
+import jakarta.validation.constraints.Null;
+import org.cosmic.backend.domain.mail.exceptions.ExistEmailException;
 import org.cosmic.backend.domain.user.domain.Email;
 import org.cosmic.backend.domain.user.domain.User;
 import org.cosmic.backend.domain.user.dto.JoinRequest;
+import org.cosmic.backend.domain.user.exceptions.NotExistEmailException;
+import org.cosmic.backend.domain.user.exceptions.NotMatchConditionException;
+import org.cosmic.backend.domain.user.exceptions.NotMatchPasswordException;
+import org.cosmic.backend.domain.user.exceptions.NullException;
 import org.cosmic.backend.domain.user.repository.EmailRepository;
 import org.cosmic.backend.domain.user.repository.UsersRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
@@ -22,11 +29,11 @@ public class UserService {
 
         //데이터 검증
         if (request.getEmail() == null || request.getPassword() == null || request.getCheckPassword() == null || request.getName() == null) {
-            throw new IllegalArgumentException("빈 데이터가 있습니다.");
+            throw new NullException();
         }
 
         if (!request.getPassword().equals(request.getCheckPassword())) {
-            throw new IllegalArgumentException("비밀번호와 확인비밀번호가 일치하지않습니다.");
+            throw new NotMatchPasswordException();
         }
 
         // 이메일일치 여부 검증
@@ -34,7 +41,7 @@ public class UserService {
 
         //이메일 인증 확인
         if (emailOpt.isEmpty() || !emailOpt.get().getVerified()) {//해당 이메일이 등록된게 아니라면
-            throw new Exception("이메일이 등록되지 않았거나 없는 이메일입니다.");
+            throw new NotExistEmailException();
         }
 
         /*비밀번호 조건 확인
@@ -42,15 +49,15 @@ public class UserService {
         */
 
         if(request.getPassword().length() < 8) {
-            throw new Exception("비밀번호 형식에 맞지 않습니다..");
+            throw new NotMatchConditionException();
         }
 
         else{
             // 모든 조건 만족했으므로 유저 객체 생성 및 저장
             User newUser = new User();
             newUser.setEmail(emailOpt.get());
-            //패스워드 인코더
-            newUser.setPassword(request.getPassword());
+            BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+            newUser.setPassword(passwordEncoder.encode(request.getPassword()));
             newUser.setUsername(request.getName());
             usersRepository.save(newUser);//유저 테이블 새로 생성
 
