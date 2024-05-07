bc118dee877ebc68a0bd00d24ed1d4f678c0bde5
BiteSnail <rkwoal216@gmail.com>
2024-05-07 12:28:19 +0900

    refactor: 이메일 인증 전송 테스트 리팩토링

--- backend/src/test/java/org/cosmic/backend/domains/mail/EmailValidationTest.java
+++ backend/src/test/java/org/cosmic/backend/domains/mail/EmailValidationTest.java
@@ -5,7 +5,6 @@ import lombok.extern.log4j.Log4j2;
 import org.cosmic.backend.domain.user.domain.Email;
 import org.cosmic.backend.domain.user.domain.User;
 import org.cosmic.backend.domain.user.repository.UsersRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,9 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.time.Duration;
 import java.time.Instant;
-import java.util.TimeZone;
+import java.time.temporal.ChronoUnit;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,49 +30,39 @@ class EmailValidationTest extends EmailBaseTest {
     @Autowired
     private UsersRepository usersRepository;
 
-    @BeforeAll
-    public static void setUp(){
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-    }
-
     @Test
     public void alreadyExistEmailTest() {
         MailAddress mailAddress = new MailAddress("test1@example.com");
         ObjectMapper objectMapper = new ObjectMapper();
-        emailRepository.save(Email.builder().email(mailAddress.getEmail()).verificationCode("123456").build());
-        usersRepository.save(User.builder().email(mailAddress.getEmail()).username("testman").password("123456").profilePicture(null).build());
+        Email email = emailRepository.save(Email.builder().email(mailAddress.getEmail()).verificationCode("123456").build());
+        usersRepository.save(User.builder().email(email).username("testman").password("123456").profilePicture(null).build());
 
         try {
-            this.mockMvc.perform(post("/mail/verify")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(mailAddress)))
-                        .andDo(print())
-                        .andExpect(status().isBadRequest())
-                        .andExpect(content().string(containsString("Already exist email")));
-        } catch (Exception e){
+            this.mockMvc.perform(post("/mail/request")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(mailAddress)))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string(containsString("Already exist email")));
+        } catch (Exception e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    public void verifyEmailBeforeInterval(){
+    public void verifyEmailBeforeInterval() {
         MailAddress mailAddress = new MailAddress("twotimes@example.com");
         ObjectMapper objectMapper = new ObjectMapper();
+        emailRepository.save(Email.builder().email(mailAddress.getEmail()).verificationCode("123456").createTime(Instant.now().plus(9, ChronoUnit.HOURS)).build());
 
         try {
-            this.mockMvc.perform(post("/mail/verify")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(mailAddress)))
-                    .andDo(print())
-                    .andExpect(status().isOk());
-            assertTrue(emailRepository.findById(mailAddress.getEmail()).isPresent());
-            this.mockMvc.perform(post("/mail/verify")
+            this.mockMvc.perform(post("/mail/request")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsString(mailAddress)))
                     .andDo(print())
                     .andExpect(status().isBadRequest())
                     .andExpect(content().string(containsString("Not even 30 seconds passed")));
-        } catch (Exception e){
+        } catch (Exception e) {
             fail(e.getMessage());
         }
     }
