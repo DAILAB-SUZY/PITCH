a5aa3238cac784ce4fc3ebbc1e2a5d9bee6f9fd1
이동율 <dy9623@naver.com>
2024-05-07 11:14:00 +0900

    feat:로그인 페이지

--- frontend/react-ts/src/App.tsx
+++ frontend/react-ts/src/App.tsx
@@ -1,12 +1,8 @@
 import styled from "styled-components";
 import { Button } from "./stories/Button";
-<<<<<<< HEAD
 import Btn from "./stories/btn";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginPage from "./stories/LoginPage";
-=======
-import InputBox from "./stories/InputBox";
->>>>>>> origin/feature/frontend/init
 
 type titleType = {
   fontSize: string;
@@ -27,25 +23,11 @@ const Container = styled.div`
 
 function App() {
   return (
-<<<<<<< HEAD
     <BrowserRouter>
       <Routes>
         <Route path="" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
-=======
-    <div>
-      <Container>
-        <div>hi</div>
-        <Title fontSize="45px" margin="0px">
-          {" "}
-          test123{" "}
-        </Title>
-        <Button primary size="small" label="Log out" />
-      </Container>
-      <InputBox text="E-mail"></InputBox>
-    </div>
->>>>>>> origin/feature/frontend/init
   );
 }
 
