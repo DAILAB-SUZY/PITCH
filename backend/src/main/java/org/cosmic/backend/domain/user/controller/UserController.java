f755ec808be587aed5817f5024dcabb57f229868
김준호 <kimjunho1231@naver.com>
2024-05-09 12:53:35 +0900

    refactor: change to annotation and Exception

--- backend/src/main/java/org/cosmic/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/org/cosmic/backend/domain/user/controller/UserController.java
@@ -3,11 +3,11 @@ package org.cosmic.backend.domain.user.controller;
 import org.cosmic.backend.domain.user.dto.JoinRequest;
 import org.cosmic.backend.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
     private final UserService userService;
@@ -17,16 +17,13 @@ public class UserController {
     }
 
     //사용자로부터 회원가입 요청 받음
-    @PostMapping("/register")
+    @PostMapping("/user/register")
     public ResponseEntity<?> registerUser(@RequestBody JoinRequest request) {
         if (request == null) {
             return ResponseEntity.badRequest().body("데이터 수신못함");
         }
-        try {
-            userService.registerUser(request);
-            return ResponseEntity.ok("성공");//회원가입 완료 표시
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+
+        userService.registerUser(request);
+        return ResponseEntity.ok("성공");//회원가입 완료 표시
     }
 }
