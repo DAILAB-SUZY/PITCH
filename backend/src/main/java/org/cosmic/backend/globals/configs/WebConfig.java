89ac100745e28c48318646ae6b5729e5c74eebe5
김준호 <kimjunho1231@naver.com>
2024-05-13 16:44:12 +0900

    feat: 보안관련 함수 추가

--- backend/src/main/java/org/cosmic/backend/globals/configs/WebConfig.java
+++ backend/src/main/java/org/cosmic/backend/globals/configs/WebConfig.java
@@ -1,5 +1,6 @@
 package org.cosmic.backend.globals.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,4 +14,18 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // Allow cookies and credentials to be sent along
+        config.addAllowedOrigin("*"); // Allow all origins
+        config.addAllowedHeader("*"); // Allow all headers
+        config.addAllowedMethod("*"); // Allow all methods (POST, GET, etc.)
+
+        source.registerCorsConfiguration("/**", config); // Register CORS configuration for all URLs
+        return new CorsFilter(source);
+    }
+
 }
