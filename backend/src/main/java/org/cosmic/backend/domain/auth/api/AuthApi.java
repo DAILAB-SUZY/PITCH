c5c0cec0d2bba1f282e17ab9c0cf1144ebd3b369
BiteSnail <rkwoal216@gmail.com>
2024-05-15 19:46:55 +0900

    docs: swagger 설명 추가

--- backend/src/main/java/org/cosmic/backend/domain/auth/api/AuthApi.java
+++ backend/src/main/java/org/cosmic/backend/domain/auth/api/AuthApi.java
@@ -1,8 +1,13 @@
 package org.cosmic.backend.domain.auth.api;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.log4j.Log4j2;
 import org.cosmic.backend.domain.auth.dto.UserLogin;
 import org.cosmic.backend.domain.user.service.UserService;
+import org.cosmic.backend.globals.dto.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +24,21 @@ public class AuthApi {
     private UserService userService;
 
     @PostMapping("/signin")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = UserLogin.class))
+                    }),
+            @ApiResponse(responseCode = "401",
+                    description = "Email or Password is invalid",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    })
+    }
+    )
     public ResponseEntity<?> authenticate(@RequestBody UserLogin userLogin) {
         return ResponseEntity.ok(userService.getByCredentials(userLogin.getEmail(), userLogin.getPassword()));
     }
