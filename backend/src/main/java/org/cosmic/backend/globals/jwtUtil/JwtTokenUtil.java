78680e06bfa59ec6a4c3ef9d50b9f226e9554e09
김준호 <kimjunho1231@naver.com>
2024-05-16 11:51:31 +0900

    feat: Jwt 생성 및 유효기간 체크 함수 개발

--- backend/src/main/java/org/cosmic/backend/globals/jwtUtil/JwtTokenUtil.java
+++ backend/src/main/java/org/cosmic/backend/globals/jwtUtil/JwtTokenUtil.java
@@ -4,64 +4,41 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.util.Date;
-import java.util.function.Function;
-
-public static String createJwt(String secretKey,Long expiredMs,String memberEmail)
-{
-
-}
 
 @Component
 public class JwtTokenUtil {
 
-    @Value("${jwt.secret}")
+    @Value("${jwt.secretKey}")
     private String secretKey;
-
-    @Value("${jwt.expiration}")
+    @Value("${jwt.expiredMs}")
     private Long expiration;
 
-    public String generateToken(Authentication authentication) {
-        UserDetails user = (UserDetails) authentication.getPrincipal();
-        return Jwts.builder()
-                .setSubject(user.getUsername())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))  // Expiration time
-                .signWith(SignatureAlgorithm.HS512, secretKey)  // Use the HS512 algorithm and our secret key to sign the token
+    public String createJwt(String memberEmail)
+    {
+        Claims claims = Jwts.claims();
+        claims.put("email", memberEmail);//payload
+
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plusMillis(expiration)))//
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
+        return token;
     }
 
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    public String getUserEmail(String token)
+    {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("email",String.class);
+        //토큰의 유효성 검사하여 유효하면 email 바탕으로 repository 정보 가져옴
     }
 
-    public String extractUsername(String token) {
-        return getClaimFromToken(token, Claims::getSubject);
-    }
-
-    private Date getExpirationDateFromToken(String token) {
-        return getClaimFromToken(token, Claims::getExpiration);
-    }
-
-    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = getAllClaimsFromToken(token);
-        return claimsResolver.apply(claims);
-    }
-
-    private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser()
-                .setSigningKey(secretKey)
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    private Boolean isTokenExpired(String token) {
-        final Date expiration = getExpirationDateFromToken(token);
-        return expiration.before(new Date());
+    public boolean isExpired(String token)
+    {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getExpiration().before(Date.from(Instant.now()));
     }
 }
