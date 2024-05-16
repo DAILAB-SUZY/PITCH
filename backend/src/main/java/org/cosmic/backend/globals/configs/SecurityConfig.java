e5d29d9e490ae8f6315b8c56b217f234eb1873ce
김준호 <kimjunho1231@naver.com>
2024-05-16 11:58:52 +0900

    feat: password인코더 및 security관련 설정

--- backend/src/main/java/org/cosmic/backend/globals/configs/SecurityConfig.java
+++ backend/src/main/java/org/cosmic/backend/globals/configs/SecurityConfig.java
@@ -1,18 +1,17 @@
 package org.cosmic.backend.globals.configs;
 
-import jakarta.servlet.http.HttpServletResponse;
-import org.cosmic.backend.domain.user.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
 import org.cosmic.backend.globals.jwtUtil.JwtTokenFilter;
+import org.cosmic.backend.globals.jwtUtil.JwtTokenUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -23,92 +22,15 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final UsersRepository usersRepository;
-    private final JwtTokenFilter jwtTokenFilter;
-
-    public SecurityConfig(UsersRepository usersRepository,
-                          JwtTokenFilter jwtTokenFilter) {
-        this.usersRepository = usersRepository;
-        this.jwtTokenFilter = jwtTokenFilter;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(email -> usersRepository
-                .findByEmail_Email(email)
-                .orElseThrow(
-                        () -> new UsernameNotFoundException(
-                                format("email: %s, not found", email)
-                        )
-                ));
-    }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // TODO configure web security
-        http = http.cors().and().csrf().disable();
-
-        // Set session management to stateless
-        http = http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and();
-
-        // Set unauthorized requests exception handler
-        http = http
-                .exceptionHandling()
-                .authenticationEntryPoint(
-                        (request, response, ex) -> {
-                            response.sendError(
-                                    HttpServletResponse.SC_UNAUTHORIZED,
-                                    ex.getMessage()
-                            );
-                        }
-                )
-                .and();
-
-        // Set permissions on endpoints
-        http.authorizeRequests()
-                // Our public endpoints
-                .antMatchers("/api/public/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/author/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/author/search").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/book/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/book/search").permitAll()
-                // Our private endpoints
-                .anyRequest().authenticated();
-
-        // Add JWT token filter
-        http.addFilterBefore(
-                jwtTokenFilter,
-                UsernamePasswordAuthenticationFilter.class
-        );
-    }
-    // Used by Spring Security if CORS is enabled.
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtTokenUtil jwtTokenUtil;
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
@@ -118,7 +40,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //TODO: MUST change csrf disable to able
-        http.cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable);
+        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+
+        http.cors(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz -> authz
+                    .requestMatchers("/user/**").permitAll()
+                    .requestMatchers("/admin/**").permitAll()
+                    .anyRequest().authenticated())
+                .addFilterBefore(new JwtTokenFilter(jwtTokenUtil, authenticationManager), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+    @Configuration
+    public class BcryptConfig {
+        @Bean
+        public BCryptPasswordEncoder bCryptPasswordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
     }
 }
