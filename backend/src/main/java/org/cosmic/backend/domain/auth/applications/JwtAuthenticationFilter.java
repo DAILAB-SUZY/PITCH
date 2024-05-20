fab5df5be0abab14609b184d2f47873e249132ff
김준호 <kimjunho1231@naver.com>
2024-05-20 17:23:40 +0900

    refactor : 다음 filter chain으로 이동할 수 있게 만듦

--- backend/src/main/java/org/cosmic/backend/domain/auth/applications/JwtAuthenticationFilter.java
+++ backend/src/main/java/org/cosmic/backend/domain/auth/applications/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package org.cosmic.backend.domain.auth.applications;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
 @Slf4j
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -23,18 +26,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     TokenProvider tokenProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-    {
-
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try{
             //요청에서 토큰 가져오기
             String token=parseBearerToken(request);
-            log.info("Filter is runnig");
             //토큰 검사하기 JWT이므로 인가 서버에 요청하지 않고도 검증 가능
             if(token!=null&&!token.equalsIgnoreCase("null")){
+
                 //userID가져오기 위조된 경우 예외처리(다르다면)
                 String userId=tokenProvider.validateAndGetUserId(token);
-                log.info("User id is "+userId);
                 //인증 완료; secutiryContextHolder에 등록해야 인증된 사용자라 생각함
                 AbstractAuthenticationToken authentication=new UsernamePasswordAuthenticationToken(
                         userId,
@@ -50,6 +50,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         catch(Exception e){
             logger.error("could not set user authentication in security context",e);
         }
+        filterChain.doFilter(request,response);
     }
 
     private String parseBearerToken(HttpServletRequest request){
