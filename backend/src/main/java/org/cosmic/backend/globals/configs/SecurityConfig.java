a5bc209f7a46c3c0074ee0c3003e9c30bee76e4d
김준호 <kimjunho1231@naver.com>
2024-05-20 17:23:08 +0900

    refactor : filter chain 등록할 때 발생하는 문제 해결

--- backend/src/main/java/org/cosmic/backend/globals/configs/SecurityConfig.java
+++ backend/src/main/java/org/cosmic/backend/globals/configs/SecurityConfig.java
@@ -1,19 +1,26 @@
 package org.cosmic.backend.globals.configs;
 
+import org.cosmic.backend.domain.auth.applications.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -27,8 +34,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //TODO: MUST change csrf disable to able
-        http.cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable);
-        return http.build();
+
+        http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                )).authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/user/**").permitAll()
+                                .requestMatchers("/auth/signin/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+        //filter등록 후 매 요청마다 CorsFilter 실행한 후에 jwtAuthenticationFilter 실행한다.
+        http.addFilterAfter(
+                jwtAuthenticationFilter,
+                CorsFilter.class
+        );
+         return http.build();
     }
+    //1
 }
