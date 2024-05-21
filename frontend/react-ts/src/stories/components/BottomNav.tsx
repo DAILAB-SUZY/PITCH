62c03f1b3d8cd7a66953a564a55eb724a2fb8688
이동율 <dy9623@naver.com>
2024-05-21 11:13:23 +0900

    feat:BottomNav ui 구현

--- frontend/react-ts/src/stories/components/BottomNav.tsx
+++ frontend/react-ts/src/stories/components/BottomNav.tsx
@@ -22,9 +22,10 @@ const Button = styled.div`
   flex-direction: column;
 `;
 
-const Title = styled.div<{ fontSize: string; margin: string }>`
+const Title = styled.div<{ fontSize: string; margin: string; color: string }>`
   font-size: ${(props) => props.fontSize};
   margin: ${(props) => props.margin};
+  color: ${(props) => props.color};
   font-family: "Bd";
 `;
 
@@ -38,36 +39,37 @@ const BottomNav = ({ onClick }: NavProps) => {
           xmlns="http://www.w3.org/2000/svg"
           width="48"
           height="48"
-          fill="currentColor"
+          fill={activeNav === 1 ? colors.Main_Pink : "grey"}
           viewBox="0 0 16 16"
         >
-          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
+          <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z" />
+          <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8m0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5" />
         </svg>
-        <Title>홈</Title>
+        <Title color={activeNav === 1 ? "black" : "grey"}>앨범챗</Title>
       </Button>
-      <Button onClick={() => setActiveNav(1)}>
+      <Button onClick={() => setActiveNav(2)}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="48"
           height="48"
-          fill="currentColor"
+          fill={activeNav === 2 ? colors.Main_Pink : "grey"}
           viewBox="0 0 16 16"
         >
           <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
         </svg>
-        <Title>홈</Title>
+        <Title color={activeNav === 2 ? "black" : "grey"}>홈</Title>
       </Button>
-      <Button onClick={() => setActiveNav(1)}>
+      <Button onClick={() => setActiveNav(3)}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="48"
           height="48"
-          fill="currentColor"
+          fill={activeNav === 3 ? colors.Main_Pink : "grey"}
           viewBox="0 0 16 16"
         >
-          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
+          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
         </svg>
-        <Title>홈</Title>
+        <Title color={activeNav === 3 ? "black" : "grey"}>홈</Title>
       </Button>
     </Container>
   );
