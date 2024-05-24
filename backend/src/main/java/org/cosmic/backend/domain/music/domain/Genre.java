202c43b94db72c5b55c6179e05d245c97e87e1a9
BiteSnail <rkwoal216@gmail.com>
2024-05-24 16:49:26 +0900

    feat: 장르 enum 생성

---
+++ backend/src/main/java/org/cosmic/backend/domain/music/domain/Genre.java
@@ -0,0 +1,13 @@
+package org.cosmic.backend.domain.music.domain;
+
+public enum Genre {
+    POP,
+    ROCK,
+    HIPHOP,
+    COUNTRY,
+    CLASSIC,
+    JAZZ,
+    ELECTRONIC,
+    RNB,
+    ROCK_METAL
+}
