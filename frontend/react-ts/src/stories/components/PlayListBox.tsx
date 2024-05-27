7e9f77627a18be6f956de10bd011a675cb2e6082
이동율 <dy9623@naver.com>
2024-05-27 15:30:55 +0900

    feat:PlayListBox 좋아요, 저장, 공유 버튼 구현

--- frontend/react-ts/src/stories/components/PlayListBox.tsx
+++ frontend/react-ts/src/stories/components/PlayListBox.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 90vw;
   height: 60vh;
-  border-radius: 5%;
+  border-radius: 3%;
   background-color: black;
   background-image: linear-gradient(to top right, #fb96a5, #ffdae0);
   display: flex;
@@ -45,6 +45,29 @@ const Title = styled.div<{ fontSize: string; margin: string }>`
   color: white;
 `;
 
+const BtnBox = styled.div`
+  width: 40vw;
+  height: 5vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  position: absolute;
+  position: fixed;
+  bottom: 15vh;
+  left: 7vw;
+  right: 0;
+`;
+
+const CircleBtn = styled.div`
+  width: 40px;
+  height: 40px;
+  background-color: white;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const PlayListBox = () => {
   const items = Array.from({ length: 20 }, (_, index) => `Item ${index + 1}`);
 
@@ -59,6 +82,44 @@ const PlayListBox = () => {
           </TextBox>
         </ListBox>
       ))}
+      <BtnBox>
+        <CircleBtn>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="red"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+            />
+          </svg>
+        </CircleBtn>
+        <CircleBtn>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="lightblue"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2" />
+          </svg>
+        </CircleBtn>
+        <CircleBtn>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5" />
+          </svg>
+        </CircleBtn>
+      </BtnBox>
     </Container>
   );
 };
