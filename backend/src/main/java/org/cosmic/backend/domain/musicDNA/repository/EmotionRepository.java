3c8a2ed0d1e7919fa6b1eed16a306bfd74edfd3a
김준호 <kimjunho1231@naver.com>
2024-05-28 22:57:15 +0900

    feat: MusicDNA데이터만 담을 Repository생성

---
+++ backend/src/main/java/org/cosmic/backend/domain/musicDNA/repository/EmotionRepository.java
@@ -0,0 +1,10 @@
+package org.cosmic.backend.domain.musicDNA.repository;
+
+import org.cosmic.backend.domain.musicDNA.domain.MusicDna;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmotionRepository extends JpaRepository<MusicDna,Long> {
+    Optional<MusicDna> findByEmotionId(Long emotionId);//key로 찾기
+}
