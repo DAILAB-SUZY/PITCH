695354530ff1950be079e693900d2e35601637cb
김준호 <kimjunho1231@naver.com>
2024-05-28 22:58:27 +0900

    feat: userId로 User객체 찾는 함수 및 MusicDna 찾는 함수 생성

--- backend/src/main/java/org/cosmic/backend/domain/musicDNA/repository/DnaRepository.java
+++ backend/src/main/java/org/cosmic/backend/domain/musicDNA/repository/DnaRepository.java
@@ -1,8 +1,12 @@
 package org.cosmic.backend.domain.musicDNA.repository;
 
-import org.cosmic.backend.domain.musicDNA.domain.MusicDna;
+import org.cosmic.backend.domain.musicDNA.domain.User_Dna;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DnaRepository extends JpaRepository<MusicDna,String> {
+import java.util.List;
+import java.util.Optional;
 
+public interface DnaRepository extends JpaRepository<User_Dna,Long> {
+    Optional<User_Dna> findById(Long id);//key로 찾기
+    List<User_Dna> findAllByUserId(Long userId);
 }
