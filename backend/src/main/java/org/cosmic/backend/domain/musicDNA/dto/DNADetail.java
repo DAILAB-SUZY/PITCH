edecfe0b09d2067ccb62f8aa2efa3a07a04950f5
김준호 <kimjunho1231@naver.com>
2024-05-28 23:00:06 +0900

    feat: controller에서 받을 request를 json데이터로 받을 DTO생성

---
+++ backend/src/main/java/org/cosmic/backend/domain/musicDNA/dto/DNADetail.java
@@ -0,0 +1,8 @@
+package org.cosmic.backend.domain.musicDNA.dto;
+
+import lombok.Data;
+
+@Data
+public class DNADetail {
+    private Long dnaKey;
+}
--- backend/src/main/java/org/cosmic/backend/domain/musicDNA/dto/DnaDTO.java
+++ backend/src/main/java/org/cosmic/backend/domain/musicDNA/dto/DnaDTO.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class DNA {//사용자로부터 데이터를 받을 것임.
-    private String UserEmail;
-    private List<String> MusicDNA;
+public class DnaDTO {//사용자로부터 데이터를 받을 것임.
+    private Long key;//userkey
+    private List<DNADetail> dna = new ArrayList<>();
 }
