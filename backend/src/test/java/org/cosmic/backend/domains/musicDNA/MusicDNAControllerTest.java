9b9e894161a3aab16502fccc9a7b11d61c34c413
김준호 <kimjunho1231@naver.com>
2024-05-29 14:40:18 +0900

    update: musicDNA 제공 및 저장 테스트 성공

--- backend/src/test/java/org/cosmic/backend/domains/musicDNA/MusicDNAControllerTest.java
+++ backend/src/test/java/org/cosmic/backend/domains/musicDNA/MusicDNAControllerTest.java
@@ -1,8 +1,10 @@
 package org.cosmic.backend.domains.musicDNA;
 
 import lombok.extern.log4j.Log4j2;
-import org.cosmic.backend.domain.auth.dto.UserLogin;
-import org.cosmic.backend.domain.musicDNA.repository.DnaRepository;
+import org.cosmic.backend.domain.musicDNA.domain.MusicDna;
+import org.cosmic.backend.domain.musicDNA.dto.DNADetail;
+import org.cosmic.backend.domain.musicDNA.dto.DnaDTO;
+import org.cosmic.backend.domain.musicDNA.repository.EmotionRepository;
 import org.cosmic.backend.domain.user.domain.Email;
 import org.cosmic.backend.domain.user.domain.User;
 import org.cosmic.backend.domain.user.repository.EmailRepository;
@@ -11,69 +13,81 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Log4j2
 public class MusicDNAControllerTest {
-
-    @Autowired
-    private DnaRepository dnaRepository;
-    @Autowired
-    private UsersRepository usersRepository;
     @Autowired
     private EmailRepository emailRepository;
+    @Autowired
+    private UsersRepository usersRepository;
     @Autowired
     private MockMvc mockMvc;
 
     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
     ObjectMapper mapper = new ObjectMapper();
-
-    @BeforeEach
-    public void setUp() {
-        Email email = emailRepository.save(Email.builder()
-            .email("testman@example.com")
-            .verificationCode("123456")
-            .verified(true)
-            .build());
-
-        usersRepository.save(User.builder()
-            .email(email)
-            .username("junho")
-            .password(encoder.encode("123456"))
-            .build());
-    }
-
-    @AfterEach
-    public void tearDown() {
-        emailRepository.deleteAll();
-        usersRepository.deleteAll();
-    }
+    @Autowired
+    private EmotionRepository emotionRepository;
 
     //DNA데이터 잘 주는지
     @Test
-    public void request_DNA() throws Exception {
-        List<String> TestDNA= Arrays.asList("슬픔","느슨한","감정적인","행복한");
-        String json=mapper.writeValueAsString(TestDNA);
-        System.out.println(json);
-        mockMvc.perform(RestDocumentationRequestBuilders.post("/musicDNA")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json))
-                .andExpect(status().isOk());
+    public void request_DNATest() throws Exception {
+        List<MusicDna> TestDNA= Arrays.asList(new MusicDna("느긋한"),new MusicDna("신나는"),new MusicDna("조용한"),new MusicDna("청순한"));
+        for(int i=0;i<4;i++)
+        {
+            emotionRepository.save(TestDNA.get(i));
+        }
+        mockMvc.perform(post("/api/dna/give")
+        .contentType("application/json")
+        .content(mapper.writeValueAsString(TestDNA)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].emotion").value("느긋한"))
+        .andExpect(jsonPath("$[1].emotion").value("신나는"));
+    }
+
+    @Test
+    public void saveDNATest() throws Exception {
+        List<MusicDna> TestDNA= Arrays.asList(new MusicDna("느긋한"),new MusicDna("신나는"),new MusicDna("조용한"),new MusicDna("청순한"));
+        for(int i=0;i<4;i++)
+        {
+            emotionRepository.save(TestDNA.get(i));
+        }
+        DnaDTO dnaDTO=new DnaDTO();
+        dnaDTO.setKey(1L);
+        dnaDTO.setDna(Arrays.asList(new DNADetail(1L),new DNADetail(2L),new DNADetail(3L),new DNADetail(4L)));
+        Email email = emailRepository.save(Email.builder()
+                .email("testman@example.com")
+                .verificationCode("123456")
+                .verified(true)
+                .build());
+
+        usersRepository.save(User.builder()
+                .email(email)
+                .username("goodwill")
+                .password(encoder.encode("123456"))
+                .build());
+
+        mockMvc.perform(post("/api/dna/save")
+        .contentType("application/json")
+        .content(mapper.writeValueAsString(DnaDTO.builder()
+                .key(dnaDTO.getKey())
+                .dna(dnaDTO.getDna())
+                .build()
+             )))
+            .andDo(print())
+            .andExpect(status().isOk());
     }
 
     //DNA데이터 잘 받아서 들어가서 repository에 잘 들어갔는지
