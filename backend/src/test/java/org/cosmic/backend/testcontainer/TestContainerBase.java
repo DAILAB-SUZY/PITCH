df31f792f60223d76ff82449cd01abe4ed0859fc
BiteSnail <rkwoal216@gmail.com>
2024-05-31 17:02:08 +0900

    test: Postgresql 기반 테스트 클래스 구현

---
+++ backend/src/test/java/org/cosmic/backend/testcontainer/TestContainerBase.java
@@ -0,0 +1,22 @@
+package org.cosmic.backend.testcontainer;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public abstract class TestContainerBase {
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres:latest"));
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", postgres::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", postgres::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
