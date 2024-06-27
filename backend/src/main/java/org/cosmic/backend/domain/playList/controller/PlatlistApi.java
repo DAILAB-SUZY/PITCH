5dc02e265fb3ff7f8c0e02e4290346a7cb736921
김준호 <kimjunho1231@naver.com>
2024-06-27 19:48:29 +0900

    feat: 아티스트나 노래로 노래 찾는 API과 swagger 구현

--- backend/src/main/java/org/cosmic/backend/domain/playList/controller/PlatlistApi.java
+++ backend/src/main/java/org/cosmic/backend/domain/playList/controller/PlatlistApi.java
@@ -1,17 +1,22 @@
 package org.cosmic.backend.domain.playList.controller;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.transaction.Transactional;
-import org.cosmic.backend.domain.playList.domain.Playlist;
-import org.cosmic.backend.domain.playList.dto.playlistDTO;
-import org.cosmic.backend.domain.playList.dto.playlistGiveDto;
+import org.cosmic.backend.domain.playList.dto.*;
 import org.cosmic.backend.domain.playList.service.PlaylistService;
 import org.cosmic.backend.domain.user.dto.userDto;
+import org.cosmic.backend.globals.dto.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/playlist")
@@ -20,18 +25,69 @@ public class PlatlistApi {
     @Autowired
     private PlaylistService playlistService;
     private playlistGiveDto newplaylistGiveDto = new playlistGiveDto();
+
     @Transactional
     @PostMapping("/give")
-    public playlistGiveDto giveData(@RequestBody userDto user) {
+    public List<playlistGiveDto> giveData(@RequestBody userDto user) {
+        //없는 유저 아이디일 때 오류 발생
+        //유저의 플렝
        return playlistService.open(user.getUserid());
+
     }//특정 플레이어의 플레이리스트 가져와서 줌
 
     @PostMapping("/save")//수정한 플레이리스트를 여기 저장
-    public ResponseEntity<?> saveUserDNAData(@RequestBody playlistDTO playlist) {
+    public ResponseEntity<?> savePlaylistData(@RequestBody playlistDTO playlist) {
         // 데이터 받을 때
         Long Key= playlist.getId();
         playlistService.save(Key,playlist.getPlaylist());
-        return ResponseEntity.ok("성공");//회원가입 완료 표시
+        return ResponseEntity.ok("성공");
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class))
+                    }),
+
+            @ApiResponse(responseCode = "401",
+                    description = "Not Match Artist Name",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    }
+            )
+    }
+    )
+
+    @PostMapping("/Artistsearch")
+    public List<TrackGiveDto> searchArtist(@RequestBody ArtistDTO artist) {
+        return playlistService.searchArtist(artist.getArtistName());
+    }
+
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class))
+                    }),
+
+            @ApiResponse(responseCode = "401",
+                    description = "Not Match Track Title",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    }
+            )
+    }
+    )
+
+    @PostMapping("/Tracksearch")
+    public List<TrackGiveDto> searchTrack(@RequestBody trackDTO track) {
+        return playlistService.searchTrack(track.getTrackName());
     }
 
 }
