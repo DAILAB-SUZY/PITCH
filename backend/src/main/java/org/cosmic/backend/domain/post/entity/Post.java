a7360dd6ba5b92393f99954183754d075e8dd111
김준호 <kimjunho1231@naver.com>
2024-07-09 17:54:29 +0900

    feat : albumPost에서 post entity생성

---
+++ backend/src/main/java/org/cosmic/backend/domain/post/entity/Post.java
@@ -0,0 +1,41 @@
+package org.cosmic.backend.domain.post.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.cosmic.backend.domain.user.domain.User;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "post") // 테이블 이름 수정
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id") // 컬럼 이름 명시
+    private Long postId;
+
+    private String cover; // 앨범커버
+    private String title; // 앨범제목
+    private String content; // 내용
+    private String artistName;
+    private Instant updateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Like> likes = new ArrayList<>();
+}
