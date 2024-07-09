0a167ed3cb8500ee9c1f92ff74fc5ca8534de2b9
김준호 <kimjunho1231@naver.com>
2024-07-09 17:49:15 +0900

    feat : 유저의 모든 post를 주는 서비스 생성

--- backend/src/main/java/org/cosmic/backend/domain/post/service/PostService.java
+++ backend/src/main/java/org/cosmic/backend/domain/post/service/PostService.java
@@ -1,9 +1,20 @@
 package org.cosmic.backend.domain.post.service;
 
-import org.cosmic.backend.domain.post.dto.Album;
-import org.cosmic.backend.domain.post.dto.AlbumDto;
-import org.cosmic.backend.domain.post.dto.Artist;
-import org.cosmic.backend.domain.post.dto.Post;
+import org.cosmic.backend.domain.playList.domain.Album;
+import org.cosmic.backend.domain.playList.domain.Artist;
+import org.cosmic.backend.domain.playList.exceptions.NotFoundArtistException;
+import org.cosmic.backend.domain.playList.exceptions.NotFoundUserException;
+import org.cosmic.backend.domain.playList.repository.AlbumRepository;
+import org.cosmic.backend.domain.playList.repository.ArtistRepository;
+import org.cosmic.backend.domain.post.dto.Post.*;
+
+import org.cosmic.backend.domain.post.entity.Post;
+import org.cosmic.backend.domain.post.exception.NotFoundAlbumException;
+import org.cosmic.backend.domain.post.exception.NotFoundPostException;
+import org.cosmic.backend.domain.post.repository.PostRepository;
+import org.cosmic.backend.domain.user.domain.User;
+import org.cosmic.backend.domain.user.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,18 +24,36 @@ import java.util.List;
 @Service
 public class PostService {
 
-    public List<Post> getAllPosts(Long userId)
-    {
-        List<Post> posts = new ArrayList<>();
-        Post post = new Post();
-        post.setUserId(userId);
-        post.setCover("base");
-        post.setTitle("밤양갱");
-        post.setArtistName("비비");
-        post.setContent("안녕하세요");
-        post.setUpdateTime(Instant.now());
-        posts.add(post);
-        return posts;
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private UsersRepository userRepository;
+    @Autowired
+    private ArtistRepository artistRepository;
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    public List<PostReq> getAllPosts(Long userId)
+    { // 해당 유저의 post들을 모두 가져오는
+        List<PostReq> posts = new ArrayList<>();
+        if(!userRepository.findById(userId).isPresent())
+        {
+            throw new NotFoundUserException();
+        }
+        else{
+            List<Post> postList = postRepository.findByUser_UserId(userId);
+            for (int i = 0; i < postList.size(); i++) {
+                PostReq post = new PostReq(); // 각 iteration마다 새로운 객체 생성
+                post.setPostId(postList.get(i).getPostId());
+                post.setCover(postList.get(i).getCover());
+                post.setTitle(postList.get(i).getTitle());
+                post.setArtistName(postList.get(i).getArtistName());
+                post.setContent(postList.get(i).getContent());
+                post.setUpdateTime(postList.get(i).getUpdateTime());
+                posts.add(post);
+            }
+            return posts;
+        }
     }
 
     public Post getPostById(Long userId, Long postId) {
