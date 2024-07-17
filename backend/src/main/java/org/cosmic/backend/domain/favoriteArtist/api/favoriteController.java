28e4b1c83c34dca2afef91a518671677f8967416
김준호 <kimjunho1231@naver.com>
2024-07-17 15:56:43 +0900

    feat: favorite artist 관련 api 추가 및 각 api마다의 swagger 추가

--- backend/src/main/java/org/cosmic/backend/domain/favoriteArtist/api/favoriteController.java
+++ backend/src/main/java/org/cosmic/backend/domain/favoriteArtist/api/favoriteController.java
@@ -1,10 +1,17 @@
 package org.cosmic.backend.domain.favoriteArtist.api;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.transaction.Transactional;
 import org.cosmic.backend.domain.favoriteArtist.dto.*;
 import org.cosmic.backend.domain.favoriteArtist.service.favoriteArtistService;
+import org.cosmic.backend.domain.playList.dto.ArtistDTO;
+import org.cosmic.backend.domain.user.dto.userDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,22 +19,54 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/api/favoriteArtist")
 
 public class favoriteController {
-
     @Autowired
     private favoriteArtistService favoriteartistService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class))
+                    }),
+
+            @ApiResponse(responseCode = "404",
+                    description = "Not Found User",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    }
+            )
+    }
+    )
     @PostMapping("/give")
     @Transactional
-    public favoriteArtist givefavoriteArtistData(@RequestBody UserDto user) {
+    public favoriteArtist giveFavoriteArtistData(@RequestBody userDto user) {
         // 데이터 받을 때
-        return  favoriteartistService.givefavoriteArtistData(user.getUserId());//Long;
+        return  favoriteartistService.giveFavoriteArtistData(user.getUserId());//Long;
+        //줄때 아티스트 사진도 필요할듯 싶음
     }
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class))
+                    }),
 
+            @ApiResponse(responseCode = "404",
+                    description = "Not Found Artist",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    }
+            )
+    }
+    )
     @Transactional
     @PostMapping("/searchartist")
     public List<ArtistData> searchArtistData(@RequestBody ArtistDTO artist) {//artist이름 주면
@@ -35,29 +74,73 @@ public class favoriteController {
     }
 
     //givealbum -> 해당 아티스트의 앨범들을 보여줌
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class))
+                    }),
 
+            @ApiResponse(responseCode = "404",
+                    description = "Not Found Album",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    }
+            )
+    }
+    )
     @Transactional
     @PostMapping("/searchalbum")
-    public List<AlbumData> searchAlbumData(@RequestBody AlbumDTO album) {
-
+    public List<AlbumData> searchAlbumData(@RequestBody AlbumRequest album) {
         return favoriteartistService.searchAlbumData(album.getArtistId(),album.getAlbumName());
     }
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class))
+                    }),
 
-    //givetrack -> 앨범안의 노래들을 뽑음
-    @PostMapping("/searchtrack")
+            @ApiResponse(responseCode = "404",
+                    description = "Not Found Track",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    }
+            )
+    }
+    )    @PostMapping("/searchtrack")
     @Transactional
-    public List<TrackData> searchTrackData(@RequestBody TrackDTO track) {
-
+    public TrackData searchTrackData(@RequestBody TrackRequest track) {
         return favoriteartistService.searchTrackData(track.getAlbumId(),track.getTrackName());
     }
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class))
+                    }),
 
+            @ApiResponse(responseCode = "404",
+                    description = "Not Found User",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    }
+            )
+    }
+    )
     //노래까지 다 찾으면 확인 누르면 save로
 
     @PostMapping("/save")
     @Transactional
-    public ResponseEntity<?> savefavoriteArtistData(@RequestBody favoriteArtistDTO favoriteartist) {
+    public ResponseEntity<?> saveFavoriteArtistData(@RequestBody favoriteArtistDTO favoriteartist) {
         // 데이터 받을 때
-        favoriteartistService.savefavoriteArtistData(favoriteartist.getUserId(),favoriteartist.getArtistId(),favoriteartist.getAlbumId(),favoriteartist.getTrackId());//Long
+        favoriteartistService.saveFavoriteArtistData(favoriteartist);
         return ResponseEntity.ok("성공");//회원가입 완료 표시
     }
 }
