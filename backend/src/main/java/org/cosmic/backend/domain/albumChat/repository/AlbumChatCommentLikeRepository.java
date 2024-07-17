b36a70fc5bcc127bc542e6090446877dc8ee2940
김준호 <kimjunho1231@naver.com>
2024-07-17 20:57:10 +0900

    feat: 좋아요 갯수를 세거나 에러를 발견하기 위한 함수 추가

--- backend/src/main/java/org/cosmic/backend/domain/albumChat/repository/AlbumChatCommentLikeRepository.java
+++ backend/src/main/java/org/cosmic/backend/domain/albumChat/repository/AlbumChatCommentLikeRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface AlbumChatCommentLikeRepository extends JpaRepository<AlbumChatCommentLike,Long> {
     List<AlbumChatCommentLike> findByAlbumChatComment_AlbumChatCommentId(Long albumChatCommentId);
     Optional<AlbumChatCommentLike> findByAlbumChatComment_AlbumChatCommentIdAndUser_UserId(Long albumChatCommentId, Long userId);
+    Long countByAlbumChatComment_AlbumChatCommentId(Long albumChatCommentId);
 }
--- backend/src/main/java/org/cosmic/backend/domain/albumChat/repository/AlbumChatCommentRepository.java
+++ backend/src/main/java/org/cosmic/backend/domain/albumChat/repository/AlbumChatCommentRepository.java
@@ -4,8 +4,9 @@ import org.cosmic.backend.domain.albumChat.domain.AlbumChatComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AlbumChatCommentRepository extends JpaRepository<AlbumChatComment,Long> {
-    List<AlbumChatComment> findByAlbumChat_AlbumChatId(Long albumChatid);//key로 찾기
+    Optional<List<AlbumChatComment>> findByAlbumChat_AlbumChatId(Long albumChatid);//key로 찾기
     AlbumChatComment findByAlbumChatCommentId(Long albumChatCommentid);
 }
