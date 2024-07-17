cc863ae992946fb6c2a2199bb39dff35adc687c2
김준호 <kimjunho1231@naver.com>
2024-07-17 20:55:34 +0900

    feat: 좋아요 많은 순으로 나열하는 api구현

--- backend/src/main/java/org/cosmic/backend/domain/albumChat/api/AlbumChatController.java
+++ backend/src/main/java/org/cosmic/backend/domain/albumChat/api/AlbumChatController.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.cosmic.backend.domain.albumChat.dto.albumChat.AlbumChatDto;
+import org.cosmic.backend.domain.albumChat.dto.albumChat.AlbumChatManyLikeListResponse;
 import org.cosmic.backend.domain.albumChat.dto.albumChat.AlbumChatResponse;
 import org.cosmic.backend.domain.albumChat.service.AlbumChatService;
 import org.cosmic.backend.domain.post.dto.Post.AlbumDto;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/albumchat")
@@ -45,4 +49,28 @@ public class AlbumChatController {
         return albumChatService.getAlbumChatById(album);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Ok",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class))
+                    }),
+
+            @ApiResponse(responseCode = "404",
+                    description = "Not Found AlbumChat",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class))
+                    }
+            )
+    }
+    )
+
+    @Transactional
+    @PostMapping("/manylike")
+    public List<AlbumChatManyLikeListResponse> getAlbumChatCommentByManyLikeId(@RequestBody AlbumChatDto albumchat) {//Post가 아니라 post에서 나와야할 내용들이 리턴되야함
+        return albumChatService.getAlbumChatCommentByManyLikeId(albumchat);
+    }
+    //좋아요 많이 받은 순으로 댓글 나열
 }
