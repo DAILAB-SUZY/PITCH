3158682149c7204422110c52d15f6866439d63fd
김준호 <kimjunho1231@naver.com>
2024-07-17 21:21:24 +0900

    feat: 댓글들 정렬상태로 들어오게 만들기 위해 return타입 변형

--- backend/src/main/java/org/cosmic/backend/domain/albumChat/api/AlbumChatController.java
+++ backend/src/main/java/org/cosmic/backend/domain/albumChat/api/AlbumChatController.java
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.cosmic.backend.domain.albumChat.dto.albumChat.AlbumChatDto;
-import org.cosmic.backend.domain.albumChat.dto.albumChat.AlbumChatManyLikeListResponse;
 import org.cosmic.backend.domain.albumChat.dto.albumChat.AlbumChatResponse;
+import org.cosmic.backend.domain.albumChat.dto.comment.AlbumChatCommentResponse;
 import org.cosmic.backend.domain.albumChat.service.AlbumChatService;
 import org.cosmic.backend.domain.post.dto.Post.AlbumDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -69,7 +69,7 @@ public class AlbumChatController {
 
     @Transactional
     @PostMapping("/manylike")
-    public List<AlbumChatManyLikeListResponse> getAlbumChatCommentByManyLikeId(@RequestBody AlbumChatDto albumchat) {//Post가 아니라 post에서 나와야할 내용들이 리턴되야함
+    public List<AlbumChatCommentResponse> getAlbumChatCommentByManyLikeId(@RequestBody AlbumChatDto albumchat) {//Post가 아니라 post에서 나와야할 내용들이 리턴되야함
         return albumChatService.getAlbumChatCommentByManyLikeId(albumchat);
     }
     //좋아요 많이 받은 순으로 댓글 나열
