e3bbacaf699e381acac800516984776d41e27fec
bitesnail <rkwoal216@gmail.com>
2024-09-03 16:34:23 +0900

    feature: Genre Entity 추가

---
+++ backend/src/main/java/org/cosmic/backend/domain/playList/domains/Genre.java
@@ -0,0 +1,18 @@
+package org.cosmic.backend.domain.playList.domains;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Genre {
+    @Id
+    private String name;
+}
