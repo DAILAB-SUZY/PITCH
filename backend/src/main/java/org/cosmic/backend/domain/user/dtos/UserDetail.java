557db3be33f345ce0b3fe3df7eb77abc97991ecf
bitesnail <rkwoal216@gmail.com>
2024-09-12 14:02:23 +0900

    feature: userDetail DTO 추가

---
+++ backend/src/main/java/org/cosmic/backend/domain/user/dtos/UserDetail.java
@@ -0,0 +1,16 @@
+package org.cosmic.backend.domain.user.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetail {
+    private String id;
+    private String username;
+    private String profilePicture;
+}
