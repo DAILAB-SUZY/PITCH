492f5a96fb467d89f7d675004062aa6d3216a780
bitesnail <rkwoal216@gmail.com>
2024-10-09 13:40:05 +0900

    feature: 유저디테일 및 DNA디테일 멤버변수 추가

--- backend/src/main/java/org/cosmic/backend/domain/user/dtos/UserDetail.java
+++ backend/src/main/java/org/cosmic/backend/domain/user/dtos/UserDetail.java
@@ -1,16 +1,20 @@
 package org.cosmic.backend.domain.user.dtos;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.cosmic.backend.domain.musicDna.dtos.DnaDetail;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDetail {
-    private Long id;
-    private String username;
-    private String profilePicture;
+
+  private Long id;
+  private String username;
+  private String profilePicture;
+  private List<DnaDetail> dnas;
 }
