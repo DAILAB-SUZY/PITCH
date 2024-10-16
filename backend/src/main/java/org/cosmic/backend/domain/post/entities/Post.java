a58ed3750e4f608d7b38bef0d0bacc6e105927fb
bitesnail <rkwoal216@gmail.com>
2024-10-16 16:27:28 +0900

    refactor: Post의 생성 및 수정 시간 칼럼 이름 변경

--- backend/src/main/java/org/cosmic/backend/domain/post/entities/Post.java
+++ backend/src/main/java/org/cosmic/backend/domain/post/entities/Post.java
@@ -1,6 +1,19 @@
 package org.cosmic.backend.domain.post.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,10 +23,6 @@ import org.cosmic.backend.domain.post.dtos.Post.PostAndCommentsDetail;
 import org.cosmic.backend.domain.post.dtos.Post.PostDetail;
 import org.cosmic.backend.domain.user.domains.User;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,55 +30,56 @@ import java.util.List;
 @Builder
 @Table(name = "post") // 테이블 이름 수정
 public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id") // 컬럼 이름 명시
-    private Long postId;
 
-    private String content; // 내용
-    private Instant update_time;
-    @Builder.Default
-    private Instant create_time=Instant.now();
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "post_id") // 컬럼 이름 명시
+  private Long postId;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+  private String content; // 내용
+  private Instant updateTime;
+  @Builder.Default
+  private Instant createTime = Instant.now();
 
-    @ManyToOne
-    @JoinColumn(name = "album_id")
-    private Album album;
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
-    @Builder.Default
-    private List<PostComment> postComments = new ArrayList<>();
+  @ManyToOne
+  @JoinColumn(name = "album_id")
+  private Album album;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    private List<PostLike> postLikes = new ArrayList<>();
+  @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+  @Builder.Default
+  private List<PostComment> postComments = new ArrayList<>();
 
-    public static PostDetail toPostDetail(Post post) {
-        return PostDetail.builder()
-                .postId(post.postId)
-                .content(post.content)
-                .createAt(post.create_time)
-                .updateAt(post.update_time)
-                .album(Album.toAlbumDetail(post.album))
-                .author(User.toUserDetail(post.user))
-                .build();
-    }
+  @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+  @Builder.Default
+  private List<PostLike> postLikes = new ArrayList<>();
 
-    public static PostAndCommentsDetail toPostAndCommentDetail(Post post) {
-        return PostAndCommentsDetail.builder()
-                .postDetail(PostDetail.builder()
-                    .postId(post.postId)
-                    .content(post.content)
-                    .createAt(post.create_time)
-                    .updateAt(post.update_time)
-                    .album(Album.toAlbumDetail(post.album))
-                    .author(User.toUserDetail(post.user))
-                    .build())
-                .comments(PostComment.toCommentDetails(post.postComments))
-                .likes(post.postLikes.stream().map(like -> User.toUserDetail(like.getUser())).toList())
-                .build();
-    }
+  public static PostDetail toPostDetail(Post post) {
+    return PostDetail.builder()
+        .postId(post.postId)
+        .content(post.content)
+        .createAt(post.createTime)
+        .updateAt(post.updateTime)
+        .album(Album.toAlbumDetail(post.album))
+        .author(User.toUserDetail(post.user))
+        .build();
+  }
+
+  public static PostAndCommentsDetail toPostAndCommentDetail(Post post) {
+    return PostAndCommentsDetail.builder()
+        .postDetail(PostDetail.builder()
+            .postId(post.postId)
+            .content(post.content)
+            .createAt(post.createTime)
+            .updateAt(post.updateTime)
+            .album(Album.toAlbumDetail(post.album))
+            .author(User.toUserDetail(post.user))
+            .build())
+        .comments(PostComment.toCommentDetails(post.postComments))
+        .likes(post.postLikes.stream().map(like -> User.toUserDetail(like.getUser())).toList())
+        .build();
+  }
 }
