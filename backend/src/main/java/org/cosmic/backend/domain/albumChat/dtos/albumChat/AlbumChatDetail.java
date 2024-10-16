e650b0a82a337733327779a5edfc0e4cf7dd8819
김준호 <kimjunho1231@naver.com>
2024-10-16 17:18:20 +0900

    hotfix : activity data전달요소 조정

--- backend/src/main/java/org/cosmic/backend/domain/albumChat/dtos/albumChat/AlbumChatDetail.java
+++ backend/src/main/java/org/cosmic/backend/domain/albumChat/dtos/albumChat/AlbumChatDetail.java
@@ -8,7 +8,6 @@ import org.cosmic.backend.domain.albumChat.domains.AlbumChatComment;
 import org.cosmic.backend.domain.albumChat.domains.AlbumLike;
 import org.cosmic.backend.domain.albumChat.dtos.albumlike.AlbumChatAlbumLikeDetail;
 import org.cosmic.backend.domain.albumChat.dtos.comment.AlbumChatCommentDetail;
-import org.cosmic.backend.domain.playList.domains.Album;
 
 import java.util.List;
 
@@ -24,13 +23,20 @@ public class AlbumChatDetail {
     private List<AlbumChatCommentDetail> comments;
     private List<AlbumChatAlbumLikeDetail> albumLike;
 
-    public AlbumChatDetail(Album album) {
-        this.albumId = album.getAlbumId();
-        this.cover = album.getAlbumCover();
-        this.title = album.getTitle();
-        this.artistName = album.getArtist().getArtistName();
-        this.comments=album.getAlbumChatComments().stream().map(AlbumChatComment::toAlbumChatCommentDetail).toList();
-        this.albumLike=album.getAlbumLike().stream().map(AlbumLike::toAlbumChatAlbumLikeDetail).toList();
+    public static AlbumChatDetail from(AlbumChatComment albumChatComment) {
+        return AlbumChatDetail.builder()
+                .albumId(albumChatComment.getAlbum().getAlbumId())
+                .cover(albumChatComment.getAlbum().getAlbumCover())
+                .genre(albumChatComment.getAlbum().getGenre())
+                .title(albumChatComment.getAlbum().getTitle())
+                .artistName(albumChatComment.getAlbum().getArtist().getArtistName())
+                .comments(albumChatComment.getAlbum().getAlbumChatComments().stream().map(AlbumChatComment::toAlbumChatCommentDetail).toList())
+                .albumLike(albumChatComment.getAlbum().getAlbumLike().stream().map(AlbumLike::toAlbumChatAlbumLikeDetail).toList())
+                .build();
+    }
+
+    public static List<AlbumChatDetail> from(List<AlbumChatComment> albumChatComments) {
+        return albumChatComments.stream().map(AlbumChatDetail::from).toList();
     }
 
 }
