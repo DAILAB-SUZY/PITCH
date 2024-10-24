0655d5127b580631a4b30ea12699aebf0c134223
bitesnail <rkwoal216@gmail.com>
2024-10-24 21:34:43 +0900

    feature: 특정 유저의 following과 follwer 조회 기능 추가

--- backend/src/main/java/org/cosmic/backend/domain/user/apis/FollowApi.java
+++ backend/src/main/java/org/cosmic/backend/domain/user/apis/FollowApi.java
@@ -9,13 +9,14 @@ import org.cosmic.backend.domain.user.dtos.FollowDto;
 import org.cosmic.backend.globals.exceptions.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user/follow")
+@RequestMapping("/api/user")
 @Tag(name = "팔로우/팔로잉 기능")
 @AllArgsConstructor
 @Log4j2
@@ -23,7 +24,7 @@ public class FollowApi {
 
   private FollowService followService;
 
-  @PostMapping("/{otherUserId}")
+  @PostMapping("/follow/{otherUserId}")
   public ResponseEntity<List<FollowDto>> followOrUnfollow(@PathVariable Long otherUserId,
       @AuthenticationPrincipal Long userId) {
     try {
@@ -32,5 +33,15 @@ public class FollowApi {
       followService.follow(userId, otherUserId);
     }
     return ResponseEntity.ok(followService.followings(userId));
+  }
+
+  @GetMapping("/{userId}/following")
+  public ResponseEntity<List<FollowDto>> getFollowing(@PathVariable Long userId) {
+    return ResponseEntity.ok(followService.followings(userId));
+  }
+
+  @GetMapping("/{userId}/follower")
+  public ResponseEntity<List<FollowDto>> getFollwer(@PathVariable Long userId) {
+    return ResponseEntity.ok(followService.followers(userId));
   }
 }
