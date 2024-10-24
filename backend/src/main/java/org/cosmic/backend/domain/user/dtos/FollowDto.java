3b65ce85eb13dcb3f6d9b12c526e7db93d566eac
bitesnail <rkwoal216@gmail.com>
2024-10-24 20:18:02 +0900

    refactor: followDto 수정

--- backend/src/main/java/org/cosmic/backend/domain/user/dtos/FollowDto.java
+++ backend/src/main/java/org/cosmic/backend/domain/user/dtos/FollowDto.java
@@ -1,18 +1,27 @@
 package org.cosmic.backend.domain.user.dtos;
 
 import org.cosmic.backend.domain.user.domains.Follow;
+import org.cosmic.backend.domain.user.domains.User;
 
-public record FollowDto(Long userId) {
+public record FollowDto(Long userId, String username, String profilePicture) {
 
   static public FollowDto from(Follow follow) {
-    return new FollowDto(follow.getOther().getUserId());
+    return new FollowDto(follow.getOther().getUserId(),
+        follow.getOther().getUsername(),
+        follow.getOther().getProfilePicture());
+  }
+
+  static public FollowDto from(User user) {
+    return new FollowDto(user.getUserId(),
+        user.getUsername(),
+        user.getProfilePicture());
   }
 
   static public FollowDto following(Follow follow) {
-    return new FollowDto(follow.getOther().getUserId());
+    return FollowDto.from(follow.getOther());
   }
 
   static public FollowDto follower(Follow follow) {
-    return new FollowDto(follow.getUser().getUserId());
+    return FollowDto.from(follow.getUser());
   }
 }
