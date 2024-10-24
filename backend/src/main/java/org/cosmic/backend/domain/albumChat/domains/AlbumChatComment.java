197b5b4fa4dde1de1820ef17ead50da1c8f7d311
bitesnail <rkwoal216@gmail.com>
2024-10-24 16:55:44 +0900

    feat: AlbumChatComment에 Child추가

--- backend/src/main/java/org/cosmic/backend/domain/albumChat/domains/AlbumChatComment.java
+++ backend/src/main/java/org/cosmic/backend/domain/albumChat/domains/AlbumChatComment.java
@@ -3,7 +3,6 @@ package org.cosmic.backend.domain.albumChat.domains;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,8 +39,12 @@ public class AlbumChatComment {
 
   private String content;
 
-  @ManyToOne(fetch = FetchType.LAZY)
+  @ManyToOne
   private AlbumChatComment parentAlbumChatComment;
+
+  @OneToMany(mappedBy = "parentAlbumChatComment", cascade = CascadeType.ALL, orphanRemoval = true)
+  @Builder.Default
+  private List<AlbumChatComment> childComments = new ArrayList<>();
 
   @Column(name = "create_time")
   @Builder.Default
