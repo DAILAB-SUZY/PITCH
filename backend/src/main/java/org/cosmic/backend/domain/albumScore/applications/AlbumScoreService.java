db5ffd3fcdccc4df7994ea5bebb24b04f60522e1
BiteSnail <rkwoal216@gmail.com>
2024-11-02 20:03:47 +0900

    refactor: add 및 get score 로직 수정

--- backend/src/main/java/org/cosmic/backend/domain/albumScore/applications/AlbumScoreService.java
+++ backend/src/main/java/org/cosmic/backend/domain/albumScore/applications/AlbumScoreService.java
@@ -1,5 +1,7 @@
 package org.cosmic.backend.domain.albumScore.applications;
 
+import jakarta.transaction.Transactional;
+import java.util.List;
 import org.cosmic.backend.domain.albumScore.domains.AlbumScore;
 import org.cosmic.backend.domain.albumScore.repositorys.AlbumScoreRepository;
 import org.cosmic.backend.domain.playList.domains.Album;
@@ -11,46 +13,69 @@ import org.cosmic.backend.domain.user.domains.User;
 import org.cosmic.backend.domain.user.repositorys.UsersRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class AlbumScoreService
-{
-    private final AlbumScoreRepository albumScoreRepository;
-    private final UsersRepository usersRepository;
-    private final AlbumRepository albumRepository;
+public class AlbumScoreService {
 
-    public AlbumScoreService(AlbumScoreRepository albumScoreRepository, UsersRepository usersRepository, AlbumRepository albumRepository) {
-        this.albumScoreRepository = albumScoreRepository;
-        this.usersRepository = usersRepository;
-        this.albumRepository = albumRepository;
-    }
+  private final AlbumScoreRepository albumScoreRepository;
+  private final UsersRepository usersRepository;
+  private final AlbumRepository albumRepository;
 
-    public AlbumDetail addScore(Long albumId,Long userId,Integer score){
-        Album album=albumRepository.findById(albumId).orElseThrow(NotFoundAlbumException::new);
-        createAlbumScore(album,userId,score);
-        return AlbumDetail.from(AlbumScore.from(usersRepository.findByUserId(userId).get(),album, score));
-    }
+  public AlbumScoreService(AlbumScoreRepository albumScoreRepository,
+      UsersRepository usersRepository, AlbumRepository albumRepository) {
+    this.albumScoreRepository = albumScoreRepository;
+    this.usersRepository = usersRepository;
+    this.albumRepository = albumRepository;
+  }
 
-    public List<AlbumDetail> getScore(Long userId){
-        //해당 유저의 모든 점수 매긴 앨범들 가져오기.
-        usersRepository.findById(userId).orElseThrow(NotFoundUserException::new);
-        return AlbumDetail.fromByAlbumScore(albumScoreRepository.findByUser_UserId(userId));
-    }
+  @Transactional
+  public AlbumDetail addScore(Long albumId, Long userId, Integer score) {
+    createAlbumScore(albumId, userId, score);
+    return getScore(albumId, userId);
+  }
 
-    public void createAlbumScore(Album album, Long userId, int score){
-        if(albumScoreRepository.existsByAlbum_AlbumIdAndUser_UserId(album.getAlbumId(),userId)){//이미 존재하면
-            if(score!=0){
-                albumScoreRepository.findByAlbum_AlbumIdAndUser_UserId(album.getAlbumId(),userId).setScore(score);
-            }
-            else{
-                albumScoreRepository.delete(albumScoreRepository.findByAlbum_AlbumIdAndUser_UserId(album.getAlbumId(),userId));
-            }
-        }
-        else{//처음 만드는 상황
-            if(score!=0){
-                albumScoreRepository.save(AlbumScore.from(usersRepository.findByUserId(userId).get(),album,score));
-            }
-        }
+  public AlbumDetail getScore(Long albumId, Long userId) {
+    return AlbumDetail.from(albumScoreRepository.findByAlbum_AlbumIdAndUser_UserId(albumId, userId)
+        .orElseThrow(NotFoundAlbumException::new));
+  }
+
+  public List<AlbumDetail> getScores(Long userId) {
+    //해당 유저의 모든 점수 매긴 앨범들 가져오기.
+    usersRepository.findById(userId).orElseThrow(NotFoundUserException::new);
+    return AlbumDetail.fromByAlbumScore(albumScoreRepository.findByUser_UserId(userId));
+  }
+
+  @Transactional
+  protected void deleteAlbumScore(Long albumId, Long userId) {
+    albumScoreRepository.deleteByAlbum_AlbumIdAndUser_UserId(albumId, userId);
+  }
+
+  @Transactional
+  public void createAlbumScore(Long albumId, Long userId, Integer score) {
+    createAlbumScore(albumRepository.findById(albumId).orElseThrow(NotFoundAlbumException::new),
+        userId, score);
+  }
+
+  @Transactional
+  public void createAlbumScore(String spotifyAlbumId, Long userId, Integer score) {
+    createAlbumScore(albumRepository.findBySpotifyAlbumId(spotifyAlbumId)
+            .orElseThrow(NotFoundAlbumException::new),
+        userId, score);
+  }
+
+  private AlbumScore getOrCreateAlbumScore(Album album, Long userId) {
+    User user = usersRepository.findById(userId).orElseThrow(NotFoundUserException::new);
+    return albumScoreRepository.findByAlbumAndUser(album, user)
+        .orElseGet(() -> AlbumScore.from(user, album, 0));
+  }
+
+  @Transactional
+  protected void createAlbumScore(Album album, Long userId, Integer score) {
+    if (score.equals(0)) {
+      deleteAlbumScore(album.getAlbumId(), userId);
+      return;
     }
+    AlbumScore albumScore = getOrCreateAlbumScore(album, userId);
+    albumScore.setScore(score);
+    albumScoreRepository.save(albumScore);
+  }
 }
--- backend/src/main/java/org/cosmic/backend/domain/albumScore/repositorys/AlbumScoreRepository.java
+++ backend/src/main/java/org/cosmic/backend/domain/albumScore/repositorys/AlbumScoreRepository.java
@@ -1,15 +1,21 @@
 package org.cosmic.backend.domain.albumScore.repositorys;
 
+import java.util.List;
+import java.util.Optional;
 import org.cosmic.backend.domain.albumScore.domains.AlbumScore;
 import org.cosmic.backend.domain.albumScore.domains.AlbumScorePK;
+import org.cosmic.backend.domain.playList.domains.Album;
+import org.cosmic.backend.domain.user.domains.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 
 public interface AlbumScoreRepository extends JpaRepository<AlbumScore, AlbumScorePK> {
 
-    Boolean existsByAlbum_AlbumIdAndUser_UserId(Long albumId, Long userId);
-    AlbumScore findByAlbum_AlbumIdAndUser_UserId(Long albumId, Long userId);
-    List<AlbumScore> findByUser_UserId(Long userId);
+  Optional<AlbumScore> findByAlbum_AlbumIdAndUser_UserId(Long albumId, Long userId);
+
+  List<AlbumScore> findByUser_UserId(Long userId);
+
+  Optional<AlbumScore> findByAlbumAndUser(Album album, User user);
+
+  void deleteByAlbum_AlbumIdAndUser_UserId(Long albumId, Long userId);
 }
