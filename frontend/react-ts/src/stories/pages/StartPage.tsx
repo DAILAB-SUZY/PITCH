f45ac9559dbce29ad2e91ed0d6400ad92ceb0e4c
ljs831 <ljsljs2580@gmail.com>
2024-11-06 08:58:28 +0900

    feat : lottie animation 추가

--- frontend/react-ts/src/stories/pages/StartPage.tsx
+++ frontend/react-ts/src/stories/pages/StartPage.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import logo from '../../img/logo_withText.png';
 import { colors } from '../../styles/color';
 import { useNavigate } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import loadingLottie from '../../img/pitchLogoMove.json';
+import { useEffect } from 'react';
 
 const Container = styled.div`
   display: flex;
@@ -10,6 +13,14 @@ const Container = styled.div`
   height: 100vh;
   width: 100vw;
   background-color: ${colors.BG_white};
+`;
+
+const LottieArea = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
 `;
 
 function StartPage() {
@@ -29,16 +40,27 @@ function StartPage() {
     }
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      Start();
+    }, 5000); // 5초 후에 Start 함수 실행
+
+    return () => clearTimeout(timer); // 컴포넌트가 언마운트될 때 타이머 정리
+  }, []);
+
   return (
     <Container>
-      <img
+      {/* <img
         src={logo}
         width="100px"
         height="100px"
         onClick={() => {
           Start();
         }}
-      ></img>
+      ></img> */}
+      <LottieArea>
+        <Lottie animationData={loadingLottie} />
+      </LottieArea>
     </Container>
   );
 }
